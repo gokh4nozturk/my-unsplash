79c5a81cc1e48716859c7c0542aaeccce1a275be
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-09-17 15:15:44 +0300

    completed searching

--- src/components/SearchBox/SearchResult.js
+++ src/components/SearchBox/SearchResult.js
@@ -1,5 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router";
+
+import Navigation from "../layout/Navigation";
+
+import Button from "../Button";
+
+import {
+  StyledMain,
+  StyledCard,
+  Container,
+  StyledPopUp,
+} from "../layout/Main/styled";
 
 import Unsplash from "unsplash-js";
 
@@ -10,24 +21,84 @@ const splash = new Unsplash({
 
 const SearchResults = () => {
   const [images, setImages] = useState([]);
+  const { searchKey = "" } = useParams();
   const [page, setPage] = useState(1);
 
-  const getPhotos = (page) => {
+  const fetchData = (page) => {
     setPage(page);
-    splash.photos
-      .listPhotos(page, 20, "latest")
+    splash.search
+      .photos(searchKey, page, 30, {
+        orientation: "portrait",
+        color: "green",
+      })
       .then((e) => e.json())
       .then((data) => {
-        setImages([...images, ...data]);
-        console.log(data);
+        setImages(data.results);
+        console.log(data.results);
       });
   };
 
-  useEffect(() => {
-    getPhotos(1);
-  }, []);
+  const copyToClipboard = (str) => {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert("Copied!");
+  };
 
-  return <></>;
+  useEffect(() => {
+    fetchData(1);
+  }, [page]);
+
+  return (
+    <>
+      <Navigation />
+      <StyledMain>
+        {images.map((image) => (
+          <Container key={`${image.id}`} className="aspect-ratio-container">
+            <StyledCard
+              src={`${image.urls.regular}`}
+              className="aspect-ratio-item"
+            />
+            <StyledPopUp className="aspect-ratio-item">
+              <div className="buttons">
+                <Button
+                  buttonStyle="primary"
+                  buttonSize="small"
+                  onClick={() => copyToClipboard(image.links.html)}
+                >
+                  Copy
+                </Button>
+                <a href={`${image.links.download}`} target="_blank">
+                  download
+                </a>
+              </div>
+              <div className="description">
+                <p>{image.alt_description}</p>
+              </div>
+            </StyledPopUp>
+          </Container>
+        ))}
+      </StyledMain>
+      <div
+        className="load-more-button"
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Button
+          buttonSize="xLarge"
+          buttonStyle="primary"
+          onClick={() => fetchData(page + 1)}
+        >
+          load more
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default SearchResults;
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
 
 import { StyledSearchBox } from "./style";
 
 function SearchBox({ type }) {
+  const { push } = useHistory();
+  const { searchKey: searchKeyParam = "" } = useParams();
+  const [searchKey, setSearchKey] = useState(searchKeyParam);
+
+  const search = useCallback(() => {
+    if (!searchKey) return;
+    push(`/search/${searchKey}`);
+  }, [searchKey, push]);
+
+  useEffect(() => {
+    const newTimeout = setTimeout(search, 500);
+    return () => {
+      clearTimeout(newTimeout);
+    };
+  }, [search, searchKey]);
   return (
     <>
-      <StyledSearchBox placeholder={type}></StyledSearchBox>
+      <StyledSearchBox
+        placeholder={type}
+        value={searchKey}
+        onChange={(e) => {
+          setSearchKey(e.currentTarget.value);
+        }}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            search();
+          }
+        }}
+      ></StyledSearchBox>
     </>
   );
 }
