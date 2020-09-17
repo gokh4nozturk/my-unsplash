458b5a2d3b023c5b8fc1761f17738ad73b803a97
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-09-17 11:49:47 +0300

    router is done in Appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
+import SearchResults from "./components/SearchBox/SearchResult";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search/:searchKey">
+            <SearchResults />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
